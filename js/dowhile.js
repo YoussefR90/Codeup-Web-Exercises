e3ed10663f89bcadc98c77aca91826b222245880
Youssef Rivas <Youssef.Rivas@gmail.com>
2021-06-08 11:55:22 -0500

    "updated final ice cream exercise"

--- js/dowhile.js
+++ js/dowhile.js
@@ -22,3 +22,19 @@ do {
     console.log("I've sold this many " + totalSold);
 
 } while (totalSold < totalCanSell);
+
+// ice cream do while
+var allCones = Math.floor(Math.random() * 50) + 50;
+document.write("I have " + allCones + " cones to sell<br>");
+
+let total_cones = 0;
+
+do {
+    let cones_bought = Math.floor(Math.random() * 5) + 1;
+    if ((allCones - total_cones) < cones_bought)
+        continue;
+    document.write("They bought: " + cones_bought + "<br>");
+    total_cones = total_cones + cones_bought;
+    document.write("I have sold: " + total_cones);
+}
+while (allCones > total_cones);
