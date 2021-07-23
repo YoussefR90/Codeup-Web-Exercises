d34cc9b2cd870f999c50177382878f0e99cfeb2e
Youssef Rivas <Youssef.Rivas@gmail.com>
2021-07-23 10:07:43 -0500

    "map-filter-reduce ex"

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,3 +35,56 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+let atLeastThreeLangs = users.filter(function(user){
+    return user.languages.length >=3;
+    }
+)
+console.log(atLeastThreeLangs);
+
+let emailAddresses = users.map(function(user){
+    return user.email;
+})
+console.log(emailAddresses);
+
+let totalYearExp = users.reduce(function(accum,user){
+    return accum + user.yearsOfExperience;
+}, 0);
+ console.log(totalYearExp);
+
+ let averageExp = totalYearExp / users.length;
+ console.log(averageExp);
+
+ let longestEmail = users.reduce(function(currentLongest,user){
+     if (user.email.length> currentLongest.email.length){
+         return user;
+     }  else {
+         return currentLongest;
+     }
+ }, users[0]);
+
+ console.log(longestEmail.email);
+
+ let nameString = users.reduce(function(accum, user){
+     if(accum === ''){
+         return "Your instructors are:" + user.name
+     } else {
+         return accum + ',' + user.name;
+     }
+ }, '')
+console.log(nameString + '.');
+ let nameStringMap = users.map(function(user){
+     return user.name;
+ })
+console.log("your instructors are " + nameStringMap + '.');
+ let concatLangs = users.reduce(function(accum, user){
+     return accum.concat(user.languages);
+ }, []);
+
+ let uniqueLangs = concatLangs.reduce(function(accum, currentValue){
+     if(accum.indexOf(currentValue) === -1){
+         accum.push(currentValue);
+     }
+     return accum;
+ }, [])
+console.log(uniqueLangs);
